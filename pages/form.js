b5d4cf89fbf14b13c61f10dc7cbbb58567443f59
Loris-Beranger <lolo.beranger1@gmail.com>
2023-01-30 21:19:15 +0100

    a

--- pages/form.js
+++ pages/form.js
@@ -34,7 +34,7 @@ export default function Example () {
               alt='Your Company'
             />
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-              Sign in to your accounttttt
+              Sign in to your account
             </h2>
             <p className='mt-2 text-center text-sm text-gray-600'>
               Or{' '}
